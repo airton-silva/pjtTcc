f3f7638b09124ee2e7cd96da627cb1fc2de0a853
airton-silva <airtonsilvan89@gmail.com>
2022-08-18 17:08:31 -0300

    primeira versão do projeto

---
+++ api-metrics/app/main.py
@@ -0,0 +1,190 @@
+from cgi import print_arguments
+from prometheus_client import start_http_server, Summary, Counter, Histogram
+from typing import Tuple, Dict
+# from flask import Flask, Response, request, jsonify
+# import random
+# import time
+
+from json import loads
+from requests import get
+
+
+
+    # from json import loads
+    # from requests import get
+
+    # return loads(get(request_prometheus_api).text) 
+
+PROMETHEUS_KUBE_PROMETHEUS = 'http://200.129.62.190:30000/api/v1/query?query='
+API_PROMETHEUS = 'http://200.129.62.190:30000/api/v1/'
+
+
+METRICAS = {
+    'rede': {
+        'recebido': 'container_network_receive_bytes_total',
+        'receive': 'sum (rate (container_network_receive_bytes_total{kubernetes_io_hostname=~"^.*$"}[1m]))',
+        'sent': '- sum (rate (container_network_receive_bytes_total{kubernetes_io_hostname=~"^.*$"}[1m]))'
+        # 'receive': 'sum (rate (container_network_receive_bytes_total{kubernetes_io_hostname=~"^.*$"}[1m]))',
+        # 'receive': 'sum (rate (container_network_receive_bytes_total{kubernetes_io_hostname="dti-d610"}[1m]))',
+        # 'sent' : '- sum (rate (container_network_transmit_bytes_total{kubernetes_io_hostname="dti-d610"}[1m]))'
+    },
+
+    'Request': {
+        'request_duration_seconds_count':'sum(rate(http_request_duration_seconds_bucket{job="kube-state-metrics"}[5m]))',
+        'http_requests_total': 'http_requests_total{kubernetes_io_hostname="dti-d610", pod="teastore-webui-5d9c74d9d6-9lrw5"}[5m]',
+
+    },
+
+    'CPU':{
+        'consumo_percent_cpu': 'sum (rate (container_cpu_usage_seconds_total{id="/",kubernetes_io_hostname="dti-d610"}[1m])) / sum (machine_cpu_cores{kubernetes_io_hostname="dti-d610"}) * 100', # consumo em percentagem
+        'container_cpu_usage_seconds_total':'container_cpu_usage_seconds_total',
+        'machine_cpu_cores':"machine_cpu_cores",
+        'consumo_total_cpu':'sum (rate (container_cpu_usage_seconds_total{id="/",kubernetes_io_hostname=~"^.*$"}[1m]))',
+        'container_spec_cpu_period':'container_spec_cpu_period{kubernetes_io_hostname="dti-d610", pod="teastore-webui-5d9c74d9d6-9lrw5"}',
+        # 'usage_seconds_total':'rate(container_cpu_usage_seconds_total{kubernetes_io_hostname="dti-d610", pod="teastore-webui-5d9c74d9d6-9lrw5"}[1m])',
+        # 'usage_seconds_total_norm':'rate(container_cpu_usage_seconds_total{kubernetes_io_hostname="dti-d610", pod="teastore-webui-5d9c74d9d6-9lrw5"})',
+        # 'container_cpu_usage_seconds_total':'sum (rate (container_cpu_usage_seconds_total{image!="",name=~"^k8s_.*",container!="POD",kubernetes_io_hostname=~"dti-d610",namespace=~"default"}[1m])) by (container, pod)',
+    },
+    'Memory':{
+        # 'usage_bytes_total':'container_memory_usage_bytes{kubernetes_io_hostname="dti-d610", pod="teastore-webui-5d9c74d9d6-9lrw5"}',
+        # 'usage_bytes_total_norm':'container_memory_usage_bytes{kubernetes_io_hostname="dti-d610", pod="teastore-webui-5d9c74d9d6-9lrw5"}',
+        'machine_memory_bytes': 'sum (machine_memory_bytes{kubernetes_io_hostname=~\'^.*$\'})', # memoria da maquina 
+        'container_memoryWorking_set_bytes':'sum (container_memory_working_set_bytes{id="/",kubernetes_io_hostname="dti-d610"})', # consumo de memória in bytes
+        'consumo_percent_memory': 'sum (container_memory_working_set_bytes{id="/",kubernetes_io_hostname="dti-d610"}) / sum (machine_memory_bytes{kubernetes_io_hostname="dti-d610"}) * 100', # consumo em percentagem
+    },
+    'FileSystem':{
+        'consumo_percent_filesystem': 'sum (container_fs_usage_bytes{device=~"^/dev/[sv]d[a-z][1-9]$",id="/",kubernetes_io_hostname="dti-d610"}) / sum (container_fs_limit_bytes{device=~"^/dev/[sv]d[a-z][1-9]$",id="/",kubernetes_io_hostname="dti-d610"}) * 100',
+        'consumo_total_filesystem': 'sum (container_fs_usage_bytes{device=~\"^/dev/[sv]d[a-z][1-9]$\",id=\"/\",kubernetes_io_hostname=~\"^.*$\"})',
+        'container_fs_limit_bytes' : 'sum (container_fs_limit_bytes{device=~"^/dev/[sv]d[a-z][1-9]$",id="/",kubernetes_io_hostname=~\"^.*$\"})',
+    },
+
+    'Pods':{
+        'cpu_usage_seconds_total':'sum(rate(container_cpu_usage_seconds_total{kubernetes_io_hostname="dti-d610", pod="teastore-webui-5d9c74d9d6-9lrw5"}[1m])) by (container, pod)',
+    },
+    'histogram':{
+        'request_duration_seconds_count':'sum(rate(http_request_duration_seconds_bucket{job="kube-state-metrics"}[1m])) by (le)',
+    },
+    'process':{
+        'container_processes':'container_processes{kubernetes_io_hostname="dti-d610"}',
+    },
+    'namespace':{
+        'kube_namespace_labels':'kube_namespace_labels',
+    },
+    'node':{
+        'kube_node_info':'kube_node_info',
+    },
+
+
+
+
+}  
+
+def buscar(query: str)-> dict:
+    """Busca metricas no Prometheus.
+
+    """
+    url = PROMETHEUS_KUBE_PROMETHEUS + query
+    print(url)
+    dados = requisitar_prometheus(url)
+    dados_processados = processar_dados(dados)
+    return dados_processados
+    
+
+def search_metric(query: str, metric: str, tmp : str = '') -> dict:
+    """Busca metricas no Prometheus.
+
+    """
+    url = API_PROMETHEUS+'query_range?query='+METRICAS[query][metric]+ tmp
+    print(url)
+    dados = requisitar_prometheus(url)
+    dados_processados = processar_dados(dados)
+    # print(dados_processados)
+    return dados_processados
+
+
+def getTargets(query: str):
+    url = API_PROMETHEUS + query
+    dados = requisitar_prometheus(url)
+    dados_processados = processar_dados(dados)
+    return dados_processados
+
+
+def coletar_dados_prometheus(metrica: str, type: str, intervalo_prometheus: str = '') -> dict:
+    
+    requisicao_prometheus_api = criar_requisicao_prometheus(metrica, type, intervalo_prometheus)
+    print(requisicao_prometheus_api)
+    dados = requisitar_prometheus(requisicao_prometheus_api)
+    dados_processados = processar_dados(dados)
+    return dados_processados
+
+def criar_requisicao_prometheus(metrica: str, type: str, intervalo_prometheus: str) -> str:
+  # URL do Kube Prometheus 
+    url = PROMETHEUS_KUBE_PROMETHEUS + METRICAS[metrica][type] + intervalo_prometheus
+    
+    
+    return url
+    # return PROMETHEUS_KUBE_PROMETHEUS + METRICAS.get(metrica) + intervalo_prometheus
+
+
+def requisitar_prometheus(request_prometheus_api: str) -> dict:
+    """Coletar dados no prometheus através de um GET.
+
+        Args:
+            request_prometheus_api (str): A URL de consulta ao Prometheus
+            para uma determinada métrica.
+
+        Returns:
+            Dado bruto do Prometheus seguindo a estruturação do json (dict).
+    """
+    from json import loads
+    from requests import get
+
+    return loads(get(request_prometheus_api).text) 
+
+def processar_dados(dados: dict) -> dict:
+
+    from numpy import array
+   
+    
+    if dados.get('status') == 'success':
+        dados_processados = {
+            'status': dados.get('status'),
+            'data': array(dados.get('data')).tolist()
+        }
+    else:
+        dados_processados = {
+            'status': dados.get('status'),
+            'data': dados.get('data')
+        }
+    # print(dados_processados)
+    return dados_processados
+
+
+# def gerar_response(status, metric, type, msg= False):
+#     body ={}
+#     body['status'] = status
+#     body['metric'] = metric
+#     body['type'] = type
+#     if msg:
+#         body['msg'] = msg
+#     return jsonify(body, status=200)
+
+
+
+# def get_request_prometheus_api():
+#     return loads(get('http://localhost:9090/api/v1/query?query=sum(rate(http_requests_total[1m]))').text)
+
+
+# def getHistogram(t):
+#     REQUEST_TIME_HIST = Histogram('request_latency_seconds', 'Histogram')
+   
+
+
+# COUNTER = Counter('requests_greater_than_half_sec_total',
+#                   'number of waits greater than half a second')
+
+
+# @REQUEST_TIME_HIST.time()
+# def process_request(t):
+#     """A dummy function that takes some time."""
+#     time.sleep(t)
---
+++ api-metrics/app/routes.py
@@ -0,0 +1,223 @@
+from ast import Str
+from crypt import methods
+from dataclasses import replace
+from hmac import new
+from os import times
+# from urllib import response
+from flask import Flask, Response, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
+from flask_cors import CORS
+from main import coletar_dados_prometheus
+from main import getTargets
+from main import search_metric, buscar
+
+# from main import gerar_response
+
+
+# from ModelPreferences import ModelPreferences
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/monitor'
+
+db=SQLAlchemy(app)
+
+class ModelPreferences(db.Model):
+    __tablename__ = 'preferences'
+    id = db.Column(db.Integer, primary_key=True)
+    metric = db.Column(db.String(100))
+    type = db.Column(db.String(100))
+    create_at = db.Column(db.DateTime)
+    update_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'metric': self.metric,
+            'type': self.type,
+            'create_at': self.create_at,
+            'update_at': self.update_at
+        }
+ 
+db.create_all()
+
+# Inicio Routes Preferences
+@app.route('/preferences', methods=['GET'])
+def get_preferences():
+    preferences = ModelPreferences.query.all()
+    data = [preference.to_dict() for preference in preferences]
+    return jsonify(data)
+
+#create a new preference  
+@app.route('/preferences', methods=['POST'])
+def create_preferences():
+    data = request.get_json()
+    try:
+        preference = ModelPreferences(
+            metric=data['metric'],
+            type=data['type'],
+            create_at=data['create_at'],
+            update_at=data['update_at']
+        )
+        db.session.add(preference)
+        db.session.commit()
+        return jsonify(preference.to_dict())
+    except Exception as e:
+        return jsonify(str(e))
+
+#update a preference
+@app.route('/preferences/<int:id>', methods=['PUT'])
+def update_preferences(id):
+    data = request.get_json()
+    try:
+        preference = ModelPreferences.query.get(id)
+        preference.metric = data['metric']
+        preference.type = data['type']
+        preference.create_at = data['create_at']
+        preference.update_at = data['update_at']
+        db.session.commit()
+        return jsonify(preference.to_dict())
+    except Exception as e:
+        return jsonify(str(e))
+
+#delete a preference
+@app.route('/preferences/<int:id>', methods=['DELETE'])
+def delete_preferences(id):
+    try:
+        preference = ModelPreferences.query.get(id)
+        db.session.delete(preference)
+        db.session.commit()
+        return jsonify(preference.to_dict())
+    except Exception as e:
+        return jsonify(str(e))
+
+#get a preference by id
+@app.route('/preferences/<int:id>', methods=['GET'])
+def get_preferences_id(id):
+    preference = ModelPreferences.query.get(id)
+    if preference:
+        return jsonify(preference.to_dict())
+    else:
+        return jsonify({'message': 'Preference not found'})
+
+# Fim Routes Preferences
+
+@app.route('/')
+def index():
+    resp = getTargets('targets?state=active')
+    return resp
+
+@app.route('/percent-cpu', methods=['GET'])
+def percentCpu():
+    resp = coletar_dados_prometheus('CPU', 'consumo_percent_cpu',' ')
+    return resp   
+    #consumo_percent_cpu    
+
+@app.route('/cpu', methods=['GET'])
+def home():
+    resp = coletar_dados_prometheus('CPU', 'machine_cpu_cores')
+    return resp
+
+@app.route('/consumo-total-cpu', methods=['GET'])
+def cpuConsumoTotal():
+
+    resp = coletar_dados_prometheus('CPU', 'consumo_total_cpu',' ')
+    return resp    
+
+@app.route('/Pods', methods=['GET'])
+def pods():
+    resp = coletar_dados_prometheus('Pods', 'cpu_usage_seconds_total',' ')
+    return resp
+
+@app.route('/percent-memory', methods=['GET'])
+def percentMemory():
+    resp = coletar_dados_prometheus('Memory', 'consumo_percent_memory',' ')
+    return resp
+
+@app.route('/memoria', methods=['GET'])
+def memory():
+    resp = coletar_dados_prometheus('Memory', 'machine_memory_bytes',' ')
+    return resp
+@app.route('/memoria_usada', methods=['GET'])
+def memory_usage():
+    resp = coletar_dados_prometheus('Memory', 'container_memoryWorking_set_bytes',' ')
+    return resp
+
+@app.route('/filesystem_percent', methods=['GET'])
+def percentFilesystem():
+    resp = coletar_dados_prometheus('FileSystem', 'consumo_percent_filesystem')
+    return resp
+
+@app.route('/filesystem_usado', methods=['GET'])
+def filesystemConsume():
+    resp = coletar_dados_prometheus('FileSystem', 'consumo_total_filesystem', '')
+    return resp
+#container_fs_limit_bytes
+
+@app.route('/filesystem', methods=['GET'])
+def filesystem():
+    resp = coletar_dados_prometheus('FileSystem', 'container_fs_limit_bytes', '')
+    return resp
+
+@app.route('/memoria/<metric>', methods=['GET'])
+def memory_metric(metric):
+    print("q = ",metric)
+    resp = search_metric('Memory', metric, '&time=1654190349.862')
+    return resp
+
+@app.route('/rede/<metric>/', methods=['GET'])
+def rede_metric(metric):
+    data = datetime.now() # intante atual
+    nowForm = (str(data).replace(' ', 'T'))
+    startAt = nowForm[:23]+'Z'
+
+    interval = datetime.now() - timedelta(minutes=5) #subtraindo 5 minutos do instante atual
+    intvalForm = (str(interval).replace(' ', 'T'))
+    endAt = intvalForm[:23]+'Z'
+
+    print("start", intvalForm)
+
+    print("stop", startAt)
+    
+    rangeTime = '&start='+endAt+'&end='+startAt+'&step=15s'  
+
+    resp = search_metric('rede', metric, rangeTime)
+    return resp
+    
+
+@app.route('/rede', methods=['GET'])
+def rede():
+    resp = coletar_dados_prometheus('rede', 'recebido',' ')
+    return resp
+
+@app.route('/request', methods=['GET'])
+def requests():
+    resp = coletar_dados_prometheus('Request', 'request_duration_seconds_count',' ')
+    return resp
+
+@app.route('/histograma', methods=['GET'])
+def histograma():
+    resp = coletar_dados_prometheus('histograma', 'histograma',' ')
+    return resp
+
+@app.route('/nodes/', methods=['GET'])
+def nodes():
+    resp = coletar_dados_prometheus('node', 'kube_node_info',' ')
+    return resp   
+
+@app.route('/namespaces/', methods=['GET']) 
+def namespaces():
+    resp = coletar_dados_prometheus('namespace', 'kube_namespace_labels',' ')
+    return resp
+
+@app.route('/source/<query>', methods=['GET'])
+def source(query):
+    resp = buscar(query)
+    return resp
+    
+app.run()
