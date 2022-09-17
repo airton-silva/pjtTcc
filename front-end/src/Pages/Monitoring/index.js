9feea6ac6a82cfa80c5c4c1b5845e67d952462e6
airton-silva <airtonsilvan89@gmail.com>
2022-09-17 05:12:08 -0300

    page monitor

---
+++ front-end/src/Pages/Monitoring/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PreferencesMerge from "../../Components/PreferencesMerge";
+
+const Monitoring = () => {   
+    return (
+        <>   
+
+            <div style={{ marginTop:'20px', boxShadow:'0 4px 8px 0 rgba(97, 94, 94, 0.2), 0 6px 20px 0 rgba(22, 21, 21, 0.19)', border:'none'}}>
+                
+                <PreferencesMerge />
+            </div>        
+            
+        </>
+    )
+
+}
+
+export default Monitoring;
