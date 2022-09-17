62d6965a5830cfd5a3a71f0de379d97a5c332175
airton-silva <airtonsilvan89@gmail.com>
2022-09-17 05:04:39 -0300

    criação de componete card para listar merge de metricas

---
+++ front-end/src/Components/CardTablePreferences/CardTableMergeMetric.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Card, Table} from "react-bootstrap"
+import Formats from "../../Utils/Formats";
+
+const CardTableMergeMetric = (prop) => {
+
+    const {title, mergeMetric}= prop;
+
+
+    return (
+        <>
+            <Card className="text-center" >
+                <Card.Header as="h4">Metricas de {title}</Card.Header>
+                        <Card.Body>
+                            <Table responsive hover size="sm">
+                                
+                                <thead>
+                                    <tr>                   
+                                        <th>Container</th>
+                                        <th>Tempo</th> 
+                                        <th>Valor</th>             
+                            
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    mergeMetric.map((preference, idx)=>{
+                                        const { pod, tempo, valor } = preference;
+                                        
+                                        return (
+                                            <tr key={idx}>
+                                                <td>{pod}</td>
+                                                <td>{tempo}</td>
+                                                <td>{valor}</td>
+                                            </tr>
+                                        );
+                                        
+                                    })
+                                }                                            
+                                    
+                                </tbody>
+                    </Table>
+                </Card.Body>
+            </Card>
+        </>
+    )
+
+}
+
+export default CardTableMergeMetric;
